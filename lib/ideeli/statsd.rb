5da8fba2dd110e4d1aa0e0c035a1afcbea97c035
patrick brisbin <pbrisbin@gmail.com>
2012-05-15 15:37:01 -0400

    simplifying rewrite of everything

--- lib/ideeli/statsd.rb
+++ lib/ideeli/statsd.rb
@@ -1,8 +1,76 @@
-require 'ideeli/statsd/options'
-require 'ideeli/statsd/client'
+require 'resolv'
+require 'statsd'
+require 'yaml'
 
 module Ideeli
   module Statsd
+    class Client
+      STATSD_METHODS = [ :increment,
+                         :decrement,
+                         :count,
+                         :timing,
+                         :time,
+                         :gauge ]
 
+      class << self
+
+        # Define a delegator method for each valid statsd calls.
+        STATSD_METHODS.each do |method|
+          class_eval %[
+            def #{method}(*args, &block)
+              delegate_to_statsd(#{method.inspect}, *args, &block)
+            end
+          ]
+        end
+
+        attr_accessor :logger, :yaml_file, :host, :port, :namespaces
+
+        def configure
+          yield self if block_given?
+
+          self.yaml_file ||= '/etc/statsd_config.yaml'
+
+          if yaml = YAML::load(File.open(self.yaml_file)) rescue nil
+            self.host ||= yaml['host']
+            self.port ||= yaml['port']
+
+            # yaml attributes used to create the namespaces
+            node_type   = yaml['node_type']
+            fqdn        = yaml['fqdn']
+            application = yaml['application']
+          else
+            node_type = fqdn = application = nil
+          end
+
+          # add the default namespaces
+          self.namespaces ||= []
+          self.namespaces << [node_type, 'host', fqdn, application].compact.join('.')
+          self.namespaces << [node_type, 'app',  application].compact.join('.')
+        end
+
+        def statsd
+          @statsd ||= ::Statsd.new(Resolv.getaddress(host || '127.0.0.1'), port || 8125)
+        end
+
+        def delegate_to_statsd(method, *args, &block)
+          if namespaces && namespaces.any?
+            namespaces.each do |ns|
+              statsd.namespace = ns
+              statsd.__send__(method, *args, &block)
+            end
+          else
+            statsd.__send__(method, *args, &block)
+          end
+
+        rescue Exception => ex
+          if logger = self.logger
+            logger.error "statsd error: #{ex}"
+          else
+            $stderr.puts "statsd error: #{ex}"
+          end
+        end
+
+      end
+    end
   end
 end
---
+++ spec/ideeli_statsd_spec.rb
@@ -0,0 +1,118 @@
+require 'ideeli/statsd'
+
+describe Ideeli::Statsd::Client, "configure" do
+  before do
+    Ideeli::Statsd::Client.configure do |conf|
+      conf.host       = nil
+      conf.port       = nil
+      conf.logger     = nil
+      conf.yaml_file  = nil
+      conf.namespaces = []
+    end
+  end
+
+  it "should have defaults" do
+    Ideeli::Statsd::Client.logger.should be_nil
+    Ideeli::Statsd::Client.host.should be_nil
+    Ideeli::Statsd::Client.port.should be_nil
+    Ideeli::Statsd::Client.yaml_file.should eq('/etc/statsd_config.yaml')
+    Ideeli::Statsd::Client.namespaces.should include('host') # other components nil
+    Ideeli::Statsd::Client.namespaces.should include('app')  # other components nil
+  end
+
+  it "can be configured" do
+    Ideeli::Statsd::Client.configure do |conf|
+      conf.host       = 'host'
+      conf.port       = 10
+      conf.logger     = 'logger'
+      conf.yaml_file  = 'yaml_file'
+      conf.namespaces = ['a', 'b']
+    end
+
+    Ideeli::Statsd::Client.host.should eq('host')
+    Ideeli::Statsd::Client.port.should eq(10)
+    Ideeli::Statsd::Client.logger.should eq('logger')
+    Ideeli::Statsd::Client.yaml_file.should eq('yaml_file')
+    Ideeli::Statsd::Client.namespaces.should include('a')
+    Ideeli::Statsd::Client.namespaces.should include('b')
+  end
+
+  it "should read from yaml" do
+    Ideeli::Statsd::Client.configure do |conf|
+      conf.yaml_file = './statsd_config.yaml'
+    end
+
+    Ideeli::Statsd::Client.host.should eq('127.0.0.1')
+    Ideeli::Statsd::Client.port.should eq(8125)
+    Ideeli::Statsd::Client.namespaces.should include("www.host.localhost.ideeli_development")
+    Ideeli::Statsd::Client.namespaces.should include("www.app.ideeli_development")
+  end
+end
+
+describe Ideeli::Statsd::Client do
+  before do
+    Ideeli::Statsd::Client.configure do |conf|
+      conf.yaml_file  = './statsd_config.yaml'
+      conf.logger     = nil
+      conf.namespaces = []
+    end
+
+    @statsd = double("statsd")
+    @statsd.stub(:namespace=)
+
+    Ideeli::Statsd::Client.stub(:statsd).and_return(@statsd)
+  end
+
+  it "should delegate increment in each namespace" do
+    @statsd.should_receive(:increment).with('foo', 10).twice
+    Ideeli::Statsd::Client.increment('foo', 10)
+  end
+
+  it "should delegate decrement in each namespace" do
+    @statsd.should_receive(:decrement).with('foo', 10).twice
+    Ideeli::Statsd::Client.decrement('foo', 10)
+  end
+
+  it "should delegate count in each namespace" do
+    @statsd.should_receive(:count).with('foo', 1, 10).twice
+    Ideeli::Statsd::Client.count('foo', 1, 10)
+  end
+
+  it "should delegate timing in each namespace" do
+    @statsd.should_receive(:timing).with('foo', 100, 10).twice
+    Ideeli::Statsd::Client.timing('foo', 100, 10)
+  end
+
+
+  it "should delegate gauge in each namespace" do
+    @statsd.should_receive(:gauge).with('foo', 500, 10).twice
+    Ideeli::Statsd::Client.gauge('foo', 500, 10)
+  end
+
+  # TODO: only yield block once no matter how many namespaces
+  it "should delegate time in each namespace" do
+    a_block = lambda { true }
+
+    @statsd.should_receive(:time).with('foo', 10, &a_block).twice
+    Ideeli::Statsd::Client.time('foo', 10, &a_block)
+  end
+
+  it "shoudl use a logger" do
+    logger = double("logger")
+    logger.should_receive(:error)
+
+    Ideeli::Statsd::Client.configure do |conf|
+      conf.logger = logger
+    end
+
+    @statsd.stub(:increment).and_raise
+    Ideeli::Statsd::Client.increment('foo')
+  end
+
+  it "should use handle a nil logger" do
+    $stderr.should_receive(:puts)
+
+    @statsd.stub(:increment).and_raise
+    Ideeli::Statsd::Client.increment('foo')
+  end
+end
